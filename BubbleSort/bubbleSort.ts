b785bc119201b3daa205071decc3b9903d4c5a69
Pearce Odegard <pearceodegard@gmail.com>
2023-08-23 11:11:29 -0500

    Implemented alphabetical bubble sort

--- BubbleSort/bubbleSort.ts
+++ BubbleSort/bubbleSort.ts
@@ -1,10 +1,21 @@
 export function bubbleSortNumber(arr: number[]): void {
   for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length - i; j++) {
-      // have to use these ! operators because of the strict null checks
-      if (arr[j]! > arr[j + 1]!) {
-        const temp = arr[j]!;
-        arr[j] = arr[j + 1]!;
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+}
+
+export function bubbleSortAlphabetize(arr: string[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j].toLowerCase() > arr[j + 1].toLowerCase()) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
         arr[j + 1] = temp;
       }
     }
--- BubbleSort/index.ts
+++ BubbleSort/index.ts
@@ -1,7 +1,19 @@
-import { bubbleSortNumber } from "./bubbleSort";
+import { bubbleSortAlphabetize, bubbleSortNumber } from "./bubbleSort";
 
 const numberTestArray = [5, 4, 3, 2, 1];
 
 bubbleSortNumber(numberTestArray);
 
-console.log(numberTestArray);
+console.log("Sorted numbers: " + numberTestArray);
+
+const stringTestArray1 = ["e", "d", "c", "b", "a"];
+
+bubbleSortAlphabetize(stringTestArray1);
+
+console.log("Sorted letters: " + stringTestArray1);
+
+const stringTestArray2 = ["Pearce", "brendan", "Zach", "Amanda", "Cameron"];
+
+bubbleSortAlphabetize(stringTestArray2);
+
+console.log("Sorted names: " + stringTestArray2);
